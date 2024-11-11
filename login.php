cbc624752903318766203a654bbd55a188640c4c
hisony <sonyzaman9112@gmail.com>
2024-11-12 04:10:39 +0600

    first commit

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aiub Library Management - Login</title>
+</head>
+<body>
+<div >
+        <div>
+            AIUB Library Management
+        </div>
+        <form action="" method="">
+            
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" required autocomplete="off">
+            </div>
+            
+            
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required autocomplete="off">
+            </div>
+            
+         
+            <div>
+                <div>
+                    <a href="">Forgot password?</a>
+                </div>
+            </div>
+             
+            <div >
+                <input type="submit" value="Login">
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
+
+
+
